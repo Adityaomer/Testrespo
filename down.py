c0becf9668ce212a420e84aa1ead8da45803a624
Adityaomer <rochanaomer786@gmail.com>
2024-10-06 12:30:21 +0530

    Update down.py

--- down.py
+++ down.py
@@ -264,7 +264,7 @@ def upload_caption(update: Update, context: CallbackContext) -> int:
                 files=f" {collection_id}${file_id}"
             else:
                 files=f"{files} {file_id}"
-        context.bot.send_message(chat_id=-1002316663794,text=f"{files}:{photo_id}")
+        context.bot.send_message(chat_id=-1002316663794,text=f"{files}${photo_id}")
         bot_username = context.bot.get_me().username
 
         if file_ids:
