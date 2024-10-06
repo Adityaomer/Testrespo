adb350849c3d8684dba68b0b1028c620b19872e9
Adityaomer <rochanaomer786@gmail.com>
2024-10-06 15:20:23 +0530

    Update down.py

--- down.py
+++ down.py
@@ -266,7 +266,7 @@ def upload_caption(update: Update, context: CallbackContext) -> int:
             else:
                 if len(files) <= 2000:
                     files=f"{files} {file_id}"
-                 else:
+                else:
                      context.bot.send_message(chat_id=-1002316663794,text=f"{files}${photo_id}")
                      files="no"
         context.bot.send_message(chat_id=-1002316663794,text=f"{files}${photo_id}")
