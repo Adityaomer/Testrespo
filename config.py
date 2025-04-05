93c48eaaeaaa48b550526473d622c541b784a74e
Adityaomer <rochanaomer786@gmail.com>
2025-04-05 16:28:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 API_ID = int("23599783")
 API_HASH = "62c4987db06716e25c4d68dcdcdc1ea5"
-BOT_TOKEN = "6973255869:AAGuwrDqQEYqQ0sOZk-D4C5e_nsDff5kO2w"
-
+BOT_TOKEN = "7541028256:AAHwPTJw7SltuagihXg2hDErXJiZdKZL2zE"
 api_id = API_ID
 api_hash = API_HASH
 bot_token = BOT_TOKEN
