931df9a578c4085f8c5ba12561ad01c1156a6e19
Adityaomer <rochanaomer786@gmail.com>
2025-04-12 18:32:54 +0530

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -149,9 +149,9 @@ async def message_handler(event):
 
 
 
-async def main():
-    await client.run_until_disconnected()
+
+    
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    await client.run_until_disconnected()
