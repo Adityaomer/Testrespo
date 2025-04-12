b978cc98e066aa3636041b8fa27560285d7b6c5a
Adityaomer <rochanaomer786@gmail.com>
2025-04-12 21:12:01 +0530

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -50,7 +50,7 @@ async def add_stickers_to_image(image_bytes, sticker_bytes_list, event):
             col = i % 4   # Determine column (0 to 3)
 
             # Adjust the spacing factor to increase distance between stickers
-            spacing_factor_x = 6  # Increase this value for more horizontal space
+            spacing_factor_x = 10  # Increase this value for more horizontal space
             spacing_factor_y = 3 #Keep this value
             x = (image_width // spacing_factor_x) * (col + 1) - (sticker_width // 2)
             y = (image_height // spacing_factor_y) * (row + 1) - (sticker_height // 2)
